d09b7fc86d1cecc2d2d6646359f5eb1cf92f8311
AgustindelaVega <agustin.delavega@ing.austral.edu.ar>
2021-05-20 22:28:33 -0300

    added multiple proxies

---
+++ src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    debugger;
+    app.use('/api/auth', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true}));
+    app.use('/api/posts', createProxyMiddleware({ target: 'http://localhost:8081', changeOrigin: true}));
+};
